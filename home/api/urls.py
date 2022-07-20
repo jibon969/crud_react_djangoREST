6c27db976634d610f4601b848ba2515891472bd8
= <help.studentgrade@gmail.com>
2022-07-20 23:15:58 +0600

    added update

--- home/api/urls.py
+++ home/api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.employee_list_api_view),
     path('employee-list/', views.employee_list_api_view),
     path('add-employee/', views.add_employee_api_view),
+    path('update-employee/', views.update_employee_api_view),
 ]
